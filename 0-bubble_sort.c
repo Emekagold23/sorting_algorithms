f72e0c41ae76ca9b08463c7770e7fd9079a0ce87
Emekagold23 <onyekachigold23@gmail.com>
2024-01-27 14:31:34 -0800

    c-sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swap two ints in an array
+ * @a: first int to swap
+ * @b: second int to swap
+ */
+
+void swap_ints(int *a, int *b)
+
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - sort an array of int in ascending order
+ * @array: the array of int to sort
+ * @size: the size of the array
+ *
+ * Description: prints the array after each swap
+ */
+
+void bubble_sort(int *array, size_t size)
+
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
